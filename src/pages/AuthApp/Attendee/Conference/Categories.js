927e49a6640e95e5187cb40157cf23a50b897067
heyjaypray <hey@jaypray.com>
2020-12-04 14:08:29 -0700

    add owl carousel

--- src/pages/AuthApp/Attendee/Conference/Categories.js
+++ src/pages/AuthApp/Attendee/Conference/Categories.js
@@ -5,6 +5,10 @@ import Slider from 'react-slick';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+
 
 
 class SubbedEvents extends Component {
@@ -13,13 +17,7 @@ class SubbedEvents extends Component {
       eventIsLive: true
     }
 
-    componentDidMount(){
-      let slickListDiv = document.getElementsByClassName('slick-list')[0];
-      slickListDiv.addEventListener('wheel', event => {
-        event.preventDefault();
-        event.deltaY > 0 ? this.slider.slickNext() : this.slider.slickPrev();
-      });
-    }
+
 
     render() {
       const { user } = this.props;
@@ -38,36 +36,43 @@ class SubbedEvents extends Component {
 
 
         <div className="mr-5 ml-5">
-          <Slider {...settings} ref={slider => this.slider = slider.innerSlider}>
+          <OwlCarousel
+            className="owl-theme"
+            loop
+            margin={10}
+            nav
+          >
 
             {
               user.conferences.map((event, key) =>
-                <Col lg="12" key={key} className="mt-4 pt-2">
-                  <div className="blog position-relative overflow-hidden shadow rounded">
-                    <div className="position-relative">
-                      <img src={event.Photos[0].url} className="img-fluid rounded-top" alt="" />
-                      <div className="overlay rounded-top bg-dark"></div>
-                    </div>
-                    <div className="content p-4">
-                      <h4><Link to="#" className="title text-dark">{event.Title}</Link></h4>
-                      <div className="post-meta mt-3">
-                        <Link to={eventIsLive ? '/account/conference/' + event.id : '/conference/' + event.id } className="text-muted float-right readmore">View Event <i className="mdi mdi-chevron-right"></i></Link>
-                        <ul className="list-unstyled mb-0">
-                          <li className="list-inline-item mr-2"><Link to="#" className="text-muted like"><i className="mdi mdi-heart-outline mr-1"></i>{event.likes}</Link></li>
-                          <li className="list-inline-item"><Link to="#" className="text-muted comments"><i className="mdi mdi-comment-outline mr-1"></i>{event.blogComments}</Link></li>
-                        </ul>
+                <div className="item">
+                  <Col lg="12" key={key} className="mt-4 pt-2">
+                    <div className="blog position-relative overflow-hidden shadow rounded">
+                      <div className="position-relative">
+                        <img src={event.Photos[0].url} className="img-fluid rounded-top" alt="" />
+                        <div className="overlay rounded-top bg-dark"></div>
+                      </div>
+                      <div className="content p-4">
+                        <h4><Link to="#" className="title text-dark">{event.Title}</Link></h4>
+                        <div className="post-meta mt-3">
+                          <Link to={eventIsLive ? '/account/conference/' + event.id : '/conference/' + event.id } className="text-muted float-right readmore">View Event <i className="mdi mdi-chevron-right"></i></Link>
+                          <ul className="list-unstyled mb-0">
+                            <li className="list-inline-item mr-2"><Link to="#" className="text-muted like"><i className="mdi mdi-heart-outline mr-1"></i>{event.likes}</Link></li>
+                            <li className="list-inline-item"><Link to="#" className="text-muted comments"><i className="mdi mdi-comment-outline mr-1"></i>{event.blogComments}</Link></li>
+                          </ul>
+                        </div>
+                      </div>
+                      <div className="author">
+                        <small className="text-light user d-block">{event.Description}</small>
+                        <small className="text-light date"><i className="mdi mdi-calendar-check"></i> {event.date}</small>
                       </div>
                     </div>
-                    <div className="author">
-                      <small className="text-light user d-block">{event.Description}</small>
-                      <small className="text-light date"><i className="mdi mdi-calendar-check"></i> {event.date}</small>
-                    </div>
-                  </div>
-                </Col>
+                  </Col>
+                </div>
               )
             }
      
-          </Slider>
+          </OwlCarousel>
         </div>
 
       );
