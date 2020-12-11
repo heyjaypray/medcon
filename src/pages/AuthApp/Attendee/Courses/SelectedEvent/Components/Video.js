eb48f8584f3e539a26c0a1a686d10fc286cb223c
heyjaypray <hey@jaypray.com>
2020-12-11 16:04:13 -0700

    update

--- src/pages/AuthApp/Attendee/Courses/SelectedEvent/Components/Video.js
+++ src/pages/AuthApp/Attendee/Courses/SelectedEvent/Components/Video.js
@@ -10,10 +10,7 @@ import { selectVideo } from '../../../../../../redux/main/actions';
 const VideoPlayer = (props) => {
   const { course, index, user, selectVideo } = props;
 
-  console.log( { course });
-
-  const [playlist, setPlaylist] = useState(null);
-  const [courseId, setCourseId] = useState(course);
+  const [video, setVideo] = useState(null)
 
   const Subscribed_Course = user && user.Subscribed_Courses && user.Subscribed_Courses.filter(i => {
     if(course){
@@ -27,58 +24,48 @@ const VideoPlayer = (props) => {
   });
 
   useEffect(() => {
-
     const a = Subscribed_Course[0];
-
     if(a && a.Modules_Viewed && a.Modules_Viewed.length < 1){
-      const b = course.Modules[0];
-
+      const b = course && course.Modules && course.Modules[0];
       const c = {
         About: b.About,
         Description: b.Description,
         Title: b.Title,
         playlist_id: b.playlist_id,
+        Viewed_Videos: [b.playlist[0]]
       };
-
       a.Modules_Viewed.push(c);
-
-      console.log({ a });
-      console.log({ b });
       selectVideo(b, user, a);
+    } if(a && a.Modules_Viewed && a.Modules_Viewed.length > 0) {
+      const d = a.Modules_Viewed
+      const e = d && d[d.length -1].Viewed_Videos
+      const f = e[e.length-1]
+      if(f) {
+        setVideo(f)
+      }
     }
-
   },[Subscribed_Course, course, selectVideo, user]);
 
 
+  console.log({ video })
 
-  const playlist_id = course && course.Modules && course.Modules[0].playlist_id;
-
-  useEffect(() => {
-    console.log(playlist_id);
-    if(playlist_id){
-      axios.get(`https://cdn.jwplayer.com/v2/playlists/${playlist_id}`)
-        .then(res => {
-          const course = res.data;
-          setPlaylist(course);
-        });
-    };
-  },[playlist_id]);
-
-
-
-  console.log('playlistttt', course);
-  return (
-    <Container>
-      <h1>{props.course && props.course.Title}</h1>
-      <ReactJWPlayer
-        playerId='vWNdCs80'
-        playerScript='https://cdn.jwplayer.com/libraries/vWNdCs80.js'
-        playlist={playlist && playlist.playlist}
-      />
-      
-      <p>{props.course && props.course.Description}</p>
-    </Container>
-  );
+  if(video){
+    return (
+      <Container>
+        <h1>{props.course && props.course.Title}</h1>
+        <ReactJWPlayer
+          playerId='vWNdCs80'
+          playerScript='https://cdn.jwplayer.com/libraries/vWNdCs80.js'
+          file={video && video.sources[video.sources.length-1].file}
+        />
+        
+        <p>{props.course && props.course.Description}</p>
+      </Container>
+    );
+  } return (
+    <div>Loading</div>
+  )
+  
 };
 
 const mapStateToProps = ({ users, main }) => {
